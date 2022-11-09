0f7f1ced955fc19a13c5335a77e80ed650a73ef8
Yash Jagtap <jagtapyash2512@gmail.com>
2022-11-09 22:43:35 +0530

    configuring Next.js to allow IPFS

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: [ // configuring Next.js to allow IPFS and other file sources
+      'ipfs.infura.io',
+      'statics-polygon-lens-staging.s3.eu-west-1.amazonaws.com',
+      'lens.infura-ipfs.io',
+      'source.unsplash.com',
+      ""
+    ],
+  },
 }
 
 module.exports = nextConfig
