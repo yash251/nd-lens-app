e157ab62175651c6f9e4bac107f4efe71ac719da
Yash Jagtap <jagtapyash2512@gmail.com>
2022-11-09 22:45:25 +0530

    exploreProfiles query

--- api.js
+++ api.js
@@ -5,3 +5,26 @@ const APIURL = "https://api.lens.dev"
 export const client = new createClient({
   url: APIURL
 })
+
+export const exploreProfiles = `
+  query ExploreProfiles {
+    exploreProfiles(request: { sortCriteria: MOST_FOLLOWERS }) {
+      items {
+        id
+        name
+        bio
+        handle
+        picture {
+          ... on MediaSet {
+            original {
+              url
+            }
+          }
+        }
+        stats {
+          totalFollowers
+        }
+      }
+    }
+  }
+`
